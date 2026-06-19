20c3c10b6625ad4b1d44c3edd29e24dabf8c0e3a
Ranganayaki Senthilkumar <74526175+RanganayakiS-web@users.noreply.github.com>
2026-06-19 23:05:26 +0530

    Update create-order.js

--- api/create-order.js
+++ api/create-order.js
@@ -3,7 +3,7 @@
 // Order details are stored in Razorpay payment notes
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://vougestreet.com');
+  res.setHeader('Access-Control-Allow-Origin', 'https://voguestreet.in');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
